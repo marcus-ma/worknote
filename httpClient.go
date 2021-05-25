c019125cebc3b5c314c8251d9fc0eedebf7b5563
marcus-ma <13715499994@163.COM>
2021-05-25 14:23:57 +0800

    Update httpClient.go

--- httpClient.go
+++ httpClient.go
@@ -83,25 +83,25 @@ func testDemo(){
 	})
 	postMaps := make(map[string]interface{})
 	postMaps["body"]=map[string]interface{}{
-			"realTimeRequestType":map[string]interface{}{
-				"producttype":0,
-				"startDate":revcData["date"].(string),
-				"endDate":revcData["date"].(string),
-				"levelOfDetails":2,
-				"number":2000,
-				"order":true,
-				"performanceData":[]string{"cost"},
-				"reportType":700,
-				"statRange":2,//统计账户粒度
-				"unitOfTime":5,//5按日
-				"bstype":0,
-			},
+		"realTimeRequestType":map[string]interface{}{
+			"producttype":0,
+			"startDate":revcData["date"].(string),
+			"endDate":revcData["date"].(string),
+			"levelOfDetails":2,
+			"number":2000,
+			"order":true,
+			"performanceData":[]string{"cost"},
+			"reportType":700,
+			"statRange":2,//统计账户粒度
+			"unitOfTime":5,//5按日
+			"bstype":0,
+		},
 	}
 	
 	body,_:=Client.Request(http.MethodGet,apiUrl,HttpRequestOption{
 		Payload: postMaps,
 		Header: map[string]string{
-				"Content-Type":"application/json;charset=UTF-8",
+			"Content-Type":"application/json;charset=UTF-8",
 		},
 	})
 }
